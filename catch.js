64ffdad7c98b096aaa0687781cd7eb1c3243c649
Minty <kinostl@gmail.com>
2020-02-13 00:32:13 -0500

    Setup some basics for the idle catching

---
+++ catch.js
@@ -0,0 +1,11 @@
+//every hour
+    //loop through every room and in each one
+        //get the lcon(players)
+        //randomize the list for fairness
+        //(future update) prioritize players who have ship parts that do that
+        //loop through the players and for each one
+            //roll on the table for a fish
+            //if they catch one, add it to their score
+            //reduce the chance of that fish on the bait table
+            //inform them of their catch with a pemit
+            //continue to the next player
